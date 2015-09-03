a8b8ac1e66bd73d6e41f7a2678816c2c29dcf9ee
Milo Thurston <knirirr@gmail.com>
2015-09-03 17:32:17 +0100

    Fixed JSON search problem.

--- postgres/test_search.rb
+++ postgres/test_search.rb
@@ -3,11 +3,11 @@ w = Workflow.first()
 puts "Got workflow: #{w.id}"
 puts "Contains element: #{ w.content['elements']['nodes'][0]['data']['id']}"
 
-#w.content[:elements][:nodes].each do |n|
-#  puts "Id: #{n[:data][:id]}"
-#end
+# I couldn't work this out, and so with the assistance of stack overflow I have used raw SQL.
+# This should be OK as long as we watch out for SQL injection. 
 
-#"content->'elements'->'nodes'->'data'->'id' = ?", 'n0'
-Workflow.where("content #>> '{elements,nodes,data,id}' = 'n0'").each do |r| # other searches can be included after the first {}
+query = "select * from workflows js,json_array_elements(content->'elements'->'nodes') as node where node->'data'->>'id' = 'n0';"
+results = Workflow.find_by_sql(query)
+results.each do |r|
   puts "Got result: #{r.id}"
 end
